d498809c81f6ca03baff9474d4580c463d843791
Soma245 <135590462+Soma245@users.noreply.github.com>
2026-03-27 17:53:26 -0500

    Add files via upload

---
+++ FirstpythonProgram.py
@@ -0,0 +1,29 @@
+name = "Soma"
+age = 23
+price = 25.99
+print ("my name is : ", name)
+
+a = int("2")
+b = 4.25
+
+print(type(a))
+print(a+b)
+
+name = input ("enter your name: ")
+print("Welcome ", name)
+
+
+val = input ("enter some value: ")
+print(type(val), val)
+
+a = int (input("enter the value: "))
+b = int(input("enter the number: "))
+print("sum =", a+b)
+
+c = float(input("enter the value: "))
+print("area =", c*c )
+
+
+d = int(input("enter:"))
+e = int(input("enter the number:"))
+print(d>=e)
